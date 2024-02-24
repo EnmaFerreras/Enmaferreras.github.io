09d9b891cb5e2583542feb6eab87862b903ae900
EnmaFerreras <ferrerasfelizenmaferreras@gmail.com>
2024-02-24 07:51:14 -0400

    moví las multiplicaciones de los numeros
    en vez de dejar que todos los numeros se multipliquen abajo, hice que se hagan arriba

--- script.js
+++ script.js
@@ -14,47 +14,59 @@ function sumar(){
     if (document.getElementById("numero1").value === ""  ){
         document.getElementById("numero1").value=0;
     }
+    numero1 *= 1;
+
     if (document.getElementById("numero5").value === ""  ){
         document.getElementById("numero5").value=0;
     }
+    numero5 *= 5;
+
     if (document.getElementById("numero10").value === ""  ){
         document.getElementById("numero10").value=0;
     }
+    numero10 *= 10;
+
     if (document.getElementById("numero25").value === ""  ){
         document.getElementById("numero25").value=0;
     }
+    numero25 *= 25;
+
     if (document.getElementById("numero50").value === ""  ){
         document.getElementById("numero50").value=0;
     }
+    numero50 *= 50;
+
     if (document.getElementById("numero100").value === ""  ){
         document.getElementById("numero100").value=0;
     }
+    numero100 *= 100;
+
     if (document.getElementById("numero200").value === ""  ){
         document.getElementById("numero200").value=0;
     }
+    numero200 *= 200;
+
     if (document.getElementById("numero500").value === ""  ){
         document.getElementById("numero500").value=0;
     }
+    numero500 *= 500;
+
     if (document.getElementById("numero1000").value === ""  ){
         document.getElementById("numero1000").value=0;  
     }
+    numero1000 *= 1000;
+    
     if (document.getElementById("numero2000").value === ""  ){
         document.getElementById("numero2000").value=0;
     }
+    numero2000 *= 2000 ;
 
     
 
 
-    numero1 *= 1;
-    numero5 *= 5;
-    numero10 *= 10;
-    numero25 *= 25;
-    numero50 *= 50;
-    numero100 *= 100;
-    numero200 *= 200;
-    numero500 *= 500;
-    numero1000 *= 1000;
-    numero2000 *= 2000 ;
+    
+    
+   
 
 
     let suma=numero1 + numero5 + numero10 + numero25  + numero50 + numero100 + numero200 + numero500 + numero1000 + numero2000;
